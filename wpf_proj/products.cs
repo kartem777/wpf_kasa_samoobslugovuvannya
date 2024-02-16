da4ab3b8d57bc2e6ec50d9c8db0e96232e27c44d
Artem K <artiukraineyt@gmail.com>
2024-02-16 14:59:51 +0100

    Add project files.

---
+++ wpf_proj/products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_proj
+{
+    internal class products
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public products(string _name, int _price) { price = _price; name = _name; }
+    }
+}
